30065429a2ff3731e30c2831b6c1c4ff8f928dd5
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-08 18:04:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,49 +4,49 @@ def game_hash
   {
     home: {team_name: "Brooklyn Nets",
     colors: ["black", "white"],
-    players: => {"Alan Anderson" number:
-    shoe:
-    points:
-    rebounds:
-    assists:
-    steals:
-    blocks:
-    slam_dunks:},
-    {"Reggie Evans"number:
-    shoe:
-    points:
-    rebounds:
-    assists:
-    steals:
-    blocks:
-    slam_dunks:,
-    "Brook Lopez"number:
-    shoe:
-    points:
-    rebounds:
-    assists:
-    steals:
-    blocks:
-    slam_dunks:, 
-    "Mason Plumlee" number:
-    shoe:
-    points:
-    rebounds:
-    assists:
-    steals:
-    blocks:
-    slam_dunks:, 
-    "Jason Terry" number:
-    shoe:
-    points:
-    rebounds:
-    assists:
-    steals:
-    blocks:
-    slam_dunks:]},
+    players: => {"Alan Anderson" number: 0 
+    shoe: 16
+    points: 22
+    rebounds: 12
+    assists: 12
+    steals: 3 
+    blocks: 1 
+    slam_dunks: 1},
+    "Reggie Evans" {number: 30
+    shoe: 14
+    points: 12
+    rebounds: 12
+    assists: 12
+    steals: 12
+    blocks: 12
+    slam_dunks:7},
+    "Brook Lopez" {number: 11
+    shoe: 17
+    points: 17
+    rebounds: 19
+    assists: 10
+    steals: 3
+    blocks: 1 
+    slam_dunks: 15}, 
+    "Mason Plumlee" {number: 1
+    shoe: 19
+    points: 26
+    rebounds: 12
+    assists: 6 
+    steals: 3
+    blocks: 8 
+    slam_dunks:5}, 
+    "Jason Terry" {number: 31 
+    shoe: 15
+    points: 19
+    rebounds: 2
+    assists: 2
+    steals: 4 
+    blocks: 11
+    slam_dunks: 1},
     
     away: {team_name: "Charlotte Hornets",
-    colors: ["Turqoise", "Purpleß"],
+    colors: ["Turqoise", "Purple"],
     players: => "Jeff Adrien" {number: "4",
     shoe: "18",
     points: "10",
